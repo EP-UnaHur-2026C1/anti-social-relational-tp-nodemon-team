3dc8d67397b19ecc9f289825f77e537e39916165
Toms200319 <mezatomas63@gmail.com>
2026-05-17 19:49:19 -0300

    Creación de modelos requeridos

---
+++ src/db/migrations/20260517222908-create-comentario.js
@@ -0,0 +1,31 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Comentarios', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      Estado: {
+        type: Sequelize.BOOLEAN
+      },
+      Contenido: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Comentarios');
+  }
+};
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("UnaHur - Anti-Social net");
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT | 3001
+
+
+app.use(express.json())
+
+app.listen(PORT, (err) =>{
+    if(err){
+        console.error(err.message)
+        process.exit(1)
+    }
+    console.log(`Aplicación iniciada en puerto ${PORT}`)
+} )
