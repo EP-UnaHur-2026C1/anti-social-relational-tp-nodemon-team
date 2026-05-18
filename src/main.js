400657000986a5e1d419906a1f997ef86a919514
tobiasramirez <tobi05cabj@gmail.com>
2026-05-18 11:49:12 -0300

    indicacion a la app que use express.json() y que escuche en el puerto PORT

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
-console.log("UnaHur - Anti-Social net");
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.listen(PORT, async (err) => {
+  if (err) {
+    console.error(err.message);
+    process.exit(1);
+  } 
+  console.log(`App iniciada correctamente en el puerto ${PORT}`)
+})
