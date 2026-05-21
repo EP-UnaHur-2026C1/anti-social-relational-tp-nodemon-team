4a8dd4fe627d145ac55815bef1987e13e2c8e9a5
Toms200319 <mezatomas63@gmail.com>
2026-05-21 08:50:18 -0300

    Importación de middlewares y rutas para comment

--- src/main.js
+++ src/main.js
@@ -4,12 +4,19 @@ const app = express()
 const PORT = process.env.PORT | 3001
 const userRoute = require("./routes/user.route")
 const postRoute = require("./routes/post.route")
+const commentRoute = require("./routes/comment.route")
+
+const {validateComment, existComment} = require("./middlewares/comment.middleware")
 const db = require("./db/models")
 
 
 app.use(express.json())
 app.use("/user",userRoute)
 app.use("/post",postRoute)
+app.use("/comment", commentRoute)
+
+app.use(validateComment)
+app.use(existComment)
 
 app.listen(PORT, async(err) =>{
     if(err){
