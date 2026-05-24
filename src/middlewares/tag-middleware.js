98d146ccd0f6934aa156423ced107785a9b390db
Toms200319 <mezatomas63@gmail.com>
2026-05-24 19:02:22 -0300

    Creación de middleware para tag

---
+++ src/middlewares/tag-middleware.js
@@ -0,0 +1,19 @@
+const {Tag} = require("../db/models")
+
+const validateTag = (req, res, next) =>{
+    const id  = req.params.id
+    if(!id){
+        return res.status(400).json({message: "Debe haber un id"})
+    }
+    next()
+}
+
+const existTag = async (req, res, next) => {
+    const tag = await Tag.findByPk(req.params.id)
+    if(!tag){
+        return res.status(404).json({message: "Tag no encontrado"})
+    }
+    next()
+}
+
+module.exports = {validateTag, existTag}
