04dbe10f759851cda3aab15f6f46363b61541f04
Toms200319 <mezatomas63@gmail.com>
2026-05-24 19:10:23 -0300

    Importación de middlewares en tag.routes.js

--- src/routes/tag.route.js
+++ src/routes/tag.route.js
@@ -1,14 +1,15 @@
 const {Router} = require("express")
 const route = Router()
 const {createTag, findByPk, findAll, editTag, deleteTag} = require ("../controllers/tag.controller")
+const {validateTag, existTag} = require("../middlewares/tag-middleware")
 
-route.get("/", findByPk)
-route.get("/:id", findAll)
+route.get("/:id",validateTag, existTag, findByPk)
+route.get("/", findAll)
 
 route.post("/:nombre", createTag)
 
-route.put("/:id", editTag)
+route.put("/:id",validateTag, existTag, editTag)
 
-route.delete("/:id", deleteTag)
+route.delete("/:id",validateTag, existTag, deleteTag)
 
 module.exports = route
